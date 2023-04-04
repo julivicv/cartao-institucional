e8e9181ffa0627b3c7dbec93b6e10e098928dd2e
julivicv <julivicv@gmail.com>
2023-04-04 12:00:19 -0300

    Login Page almost ready

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,40 @@
 import "../styles/Login.css"
 import {useState} from "react";
-import {useForm, Resolver} from "react-hook-form";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup"
+import * as Yup from "yup"
 import axios from "axios";
+import {Eye, EyeClosed} from "@phosphor-icons/react";
 
 type FormValues = {
-    firstName: string;
-    lastName: string;
+    email: string;
+    password: string;
 };
-const resolver: Resolver<FormValues> = async (values) => {
-    return {
-        values: values.Email ? values : {},
-        errors: !values.Email
-            ? {
-                firstName: {
-                    type: 'required',
-                    message: 'This is required.',
-                },
-            }
-            : {},
-    };
-};
+
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({ resolver });
-    const onSubmit = handleSubmit((data) => console.log(data));
+    const [shownPassword, setShownPassword] = useState(false)
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .required('Digite seu e-mail')
+            .email('E-mail inválido'),
+        password: Yup.string()
+            .required('Digite sua senha')
+    })
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors}
+    } = useForm<FormValues>({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = (data: FormValues) => {
+        console.log(JSON.stringify(data, null, 2));
+    };
 
     /*axios({
         method: "post",
@@ -39,24 +49,35 @@ export default function Login() {
             <div className={"login-container"}>
                 <div className={"form-container"}>
                     <h1 className={"login-title"}>Login</h1>
-                    <form action={onSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <label className={"mail-label"}>
-                            <input type={"text"} className={"mail"}
-                                   {...register("Email", {required: true, pattern: /^\S+@\S+$/i})}
+                            <input type={"text"} className={`mail ${!errors.email ? '' : 'mail-invalid'}`}
+                                   {...register("email")}
                                    onChange={(event) => setEmail(event.target.value)}
                                    value={email}
-
                             />
+                            <div className={"invalid-feedback"}>{errors.email?.message}</div>
                             <span className={email != "" ? "mail-text filled" : "mail-text"}>E-mail</span>
                         </label>
                         <label className={"pass-label"}>
-                            <input type={"password"} className={"pass"}
-                                   {...register("Password", {required: true, minLength: 6, maxLength: 70})}
+                            <input type={shownPassword ? 'text' : 'password'}
+                                   className={`pass ${!errors.password ? '' : 'pass-invalid'} ${shownPassword ? 'shown' : ''}}`}
+                                   {...register("password")}
                                    onChange={(event) => setPassword(event.target.value)}
                                    value={password}
                             />
+
+                            <div className={`invalid-feedback`}>{errors.password?.message}</div>
                             <span className={password != "" ? "pass-text filled" : "pass-text"}>Senha</span>
                         </label>
+                        <button
+                            onClick={(event) => setShownPassword(!shownPassword)}
+                            type={"button"}
+                            className={"eye-button"}>
+                            {shownPassword
+                                ? <Eye className={"eye"} size={32} weight="duotone"/>
+                                : <EyeClosed className={"eye"} size={32} weight="duotone"/>}
+                        </button>
                         <button className={"form-button"} type={"submit"}>
                             Logar-se
                         </button>
